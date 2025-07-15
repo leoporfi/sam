89027a0df977eba5c5047903a9f753512e4cea52
leonardo.porfirio <leonardo.porfirio@tmoviles.com.ar>
2025-07-15 13:03:54 -0300

    Refactorizar: mejorar la presentación de los modales y optimizar el estilo de los componentes de la interfaz

--- src/interfaz_web/components/robot_row.py
+++ src/interfaz_web/components/robot_row.py
@@ -1,11 +1,9 @@
 # src/interfaz_web/components/robot_row.py
-import asyncio
 from typing import Callable
 
 from reactpy import component, event, html
 
 from ..client.schemas.robot_types import Robot
-from .common.action_menu import ActionMenu
 
 
 @component
@@ -76,5 +74,42 @@ def RobotRow(robot: Robot, on_action: Callable):
         html.td("Programado" if robot.get("TieneProgramacion") else "A Demanda"),
         html.td(str(robot["PrioridadBalanceo"])),
         html.td(str(robot.get("TicketsPorEquipoAdicional", "N/A"))),
-        html.td(ActionMenu(actions=actions)),
+        # html.td(ActionMenu(actions=actions)),
+        html.td(
+            # Usamos un div con la clase grid para alinear los íconos horizontalmente
+            html.div(
+                {"className": "grid"},
+                # Botón/Icono para Editar
+                html.a(
+                    {
+                        "href": "#",
+                        "onClick": event(handle_edit, prevent_default=True),
+                        "data-tooltip": "Editar Robot",  # Tooltip de Pico.css
+                        "className": "secondary",
+                    },
+                    html.i({"className": "fa-solid fa-pencil"}),
+                ),
+                # Botón/Icono para Asignar
+                html.a(
+                    {
+                        "href": "#",
+                        "onClick": event(handle_assign, prevent_default=True),
+                        "data-tooltip": "Asignar Equipos",  # Tooltip de Pico.css
+                        "className": "secondary",
+                    },
+                    html.i({"className": "fa-solid fa-users"}),
+                ),
+                # Botón/Icono para Programar
+                html.a(
+                    {
+                        "href": "#",
+                        "onClick": event(handle_schedule, prevent_default=True),
+                        "data-tooltip": "Programar Tareas",
+                        "data-placement": "left",  # Tooltip de Pico.css
+                        "className": "secondary",
+                    },
+                    html.i({"className": "fa-solid fa-clock"}),
+                ),
+            )
+        ),
     )
