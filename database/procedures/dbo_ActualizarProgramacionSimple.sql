ded87f2aee27aa680af3212a4409794b2da9ea22
LPORFIRI <leonardo.porfirio@tmoviles.com.ar>
2025-12-26 17:42:37 -0300

    feat: IntegraciÃ³n de estructura de base de datos en reemplazo de SAM.sql
    - Agregada estructura completa de base de datos organizada por tipo:
      - Stored Procedures (35+ procedures)
      - Tablas (11 tablas)
      - Vistas (6 vistas)
    - Script de exportaciÃ³n PowerShell para generar estructura tipo SSMS
    - DocumentaciÃ³n README.md con descripciÃ³n de la estructura
    - Reemplaza el archivo monolÃ­tico SAM.sql por estructura modular
    - Facilita mantenimiento y versionado de objetos de base de datos

---
+++ database/procedures/dbo_ActualizarProgramacionSimple.sql
@@ -0,0 +1,51 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ActualizarProgramacionSimple]') AND type in (N'P', N'PC'))
+BEGIN
+EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[ActualizarProgramacionSimple] AS' 
+END
+
+ALTER PROCEDURE [dbo].[ActualizarProgramacionSimple]
+    @ProgramacionId INT,
+    @TipoProgramacion NVARCHAR(20),
+    @HoraInicio TIME,
+    @DiasSemana NVARCHAR(20) = NULL,
+    @DiaDelMes INT = NULL,
+    @FechaEspecifica DATE = NULL,
+    @Tolerancia INT = NULL,
+    @Activo BIT,
+    @DiaInicioMes INT = NULL,
+    @DiaFinMes INT = NULL,
+    @UltimosDiasMes INT = NULL,
+    -- NUEVOS PARÁMETROS para robots cíclicos con ventanas
+    @EsCiclico BIT = NULL,
+    @HoraFin TIME = NULL,
+    @FechaInicioVentana DATE = NULL,
+    @FechaFinVentana DATE = NULL,
+    @IntervaloEntreEjecuciones INT = NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE dbo.Programaciones
+    SET 
+        TipoProgramacion = @TipoProgramacion,
+        HoraInicio = @HoraInicio,
+        HoraFin = @HoraFin,
+        DiasSemana = CASE WHEN @TipoProgramacion = 'Semanal' THEN @DiasSemana ELSE NULL END,
+        DiaDelMes = CASE WHEN @TipoProgramacion = 'Mensual' THEN @DiaDelMes ELSE NULL END,
+        FechaEspecifica = CASE WHEN @TipoProgramacion = 'Especifica' THEN @FechaEspecifica ELSE NULL END,
+        Tolerancia = @Tolerancia,
+        Activo = @Activo,
+        DiaInicioMes = CASE WHEN @TipoProgramacion = 'RangoMensual' THEN @DiaInicioMes ELSE NULL END,
+        DiaFinMes = CASE WHEN @TipoProgramacion = 'RangoMensual' THEN @DiaFinMes ELSE NULL END,
+        UltimosDiasMes = CASE WHEN @TipoProgramacion = 'RangoMensual' THEN @UltimosDiasMes ELSE NULL END,
+        EsCiclico = ISNULL(@EsCiclico, EsCiclico),  -- Solo actualizar si se proporciona
+        FechaInicioVentana = @FechaInicioVentana,
+        FechaFinVentana = @FechaFinVentana,
+        IntervaloEntreEjecuciones = @IntervaloEntreEjecuciones,
+        FechaModificacion = GETDATE()
+    WHERE
+        ProgramacionId = @ProgramacionId;
+END
+
